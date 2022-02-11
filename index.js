9a7a5e994ed7b541fe4e664a3fff3289a8231d40
GrundeFAA <aasen.grunde@gmail.com>
2022-02-11 22:17:54 +0100

    test med last inn pokemon knapp

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const app = express();
 const PORT = 3000;
 console.log(pokemon.length);
 // add middlewares
-app.use(express.static(path.join(__dirname, "..", "rt-hub/build")));
-app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, "..", "rt-hub/build")));
+// app.use(express.static("public"));
+//
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+//   next();
+// });
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-  next();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/pokemon", (req, res) => {
