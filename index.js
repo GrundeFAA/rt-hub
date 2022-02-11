5e4ba7ca126b27a9e8544e430686d085d88ee8f5
GrundeFAA <aasen.grunde@gmail.com>
2022-02-11 22:03:51 +0100

    test med stor json

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-
 const express = require("express");
+const path = require("path");
+const pokemon = require("./pokemon.json");
 
 const app = express();
 const PORT = 3000;
+console.log(pokemon.length);
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "rt-hub/build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  next();
+});
+
+app.get("/pokemon", (req, res) => {
+  res.json(pokemon);
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-});
-
-app.use("/", (req, res) => {
-  res.end("HELLO WORLD");
 });
