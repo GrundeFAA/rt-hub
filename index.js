ce7e04114476f443d35dbfd156d26c730abcefce
GrundeFAA <aasen.grunde@gmail.com>
2022-02-11 19:58:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+app.use("/", (req, res) => {
+  res.end("HELLO WORLD");
+});
